d4e0d854b7c8102028e73fd0d468b94f34397c8b
Pawel Warmuth <warmuth.pawel@gmail.com>
2023-11-09 21:18:28 +0100

    splitted method

--- src/cloud/s3.rs
+++ src/cloud/s3.rs
@@ -1,9 +1,18 @@
 use aws_sdk_s3 as s3;
 use s3::primitives::{ByteStream, SdkBody};
-use std::{fs};
+use std::{path::Path};
+use walkdir::{DirEntry};
 
-pub async fn process_file(entry: walkdir::DirEntry) -> Result<(),()> {    
-    let file_stream = ByteStream::from_path(entry.path()).await;
+pub async fn process_file(entry: DirEntry) -> Result<(),()> {  
+    // make checks 
+    // is file already on S3 Bucket ?
+    // is it archived already ?  
+    return put_file(entry.path()).await;
+    
+} 
+
+async fn  put_file(path:&Path) -> Result<(),()> {
+    let file_stream = ByteStream::from_path(path).await;
     let stream_data = match file_stream {
         Ok(stream_data) => stream_data,
         Err(_) => return Err(())
@@ -16,5 +25,4 @@ pub async fn process_file(entry: walkdir::DirEntry) -> Result<(),()> {
         Ok(_) => return Ok(()),
         Err(_) => return Err(())
     };
-    
-} 
+}
